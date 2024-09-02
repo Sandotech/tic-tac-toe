65832436b426b905394d3cb033c99cf47a10b2f0
Diego Santos <dseosantos76@gmail.com>
2024-09-01 22:40:23 -0400

    feat: cheak won in board by character

--- lib/table.rb
+++ lib/table.rb
@@ -1,13 +1,13 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
-class Square
-  attr_reader :over?
+class Board
+  attr_reader :over
 
-  WON_GAME = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]
+  WON_GAME = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
   def initialize
     @table = Array.new(3) { Array.new(3, '-') }
-    @over? = false
+    @over = false
   end
 
   def display
@@ -22,7 +22,21 @@ class Square
     @table[row][column] = word
   end
 
+  def checkWon?(character)
+    # Receive a character and check if there are matches in WON_GAME
+    WON_GAME.select do |win_combination|
+      win_combination.all? { |index| @table[index / 3][index % 3] == character }
+    end
 
+    @over = true
+  end
 end
 
+board = Board.new
+board.setWord("X", 0, 0)
+board.setWord("X", 0, 1)
+board.setWord("X", 0, 2)
+
+puts board.checkWon?('X')
+board.display
 # "- | - | -\n- | - | -\n- | - | -"
